f2a32a736aeb18a82fd2d77b8858b078f05a2b5c
Tal Waserman <talwaserman@gmail.com>
2015-02-25 21:29:42 +0200

    clean up

--- public/javascripts/controllers/controllers.js
+++ public/javascripts/controllers/controllers.js
@@ -56,57 +56,7 @@ mainApp.controller('completeTheWordCTR',['$scope','$location','$http', function(
                 var div = $('<img src="'+imageLocation+'" style="float:right;width:60px;height:60px">');
                 $('.completeWord').append(div);
 
-                /*var snd = new Audio("./voice/"+event.target.src.split('/')[5].split('.')[0]+".mp3");
-                snd.play();
-
-                if($('.enlargedLatter img').length > 0)
-                {
-                    $('.enlargedLatter img').remove();
-                }
-                if(event.target.src)
-                {
-                    var arr = event.target.src.split("/");
-                }
-                var imageLocation =  "images/letters/"+arr[arr.length-1];
-                var div = $('<img src="'+imageLocation+'" style="width:448px;height:285px">');
-                $('.enlargedLatter').append(div);*/
             }
-
-
-
-
-
-            //compare image and letter
-            /*var tempArr = $('.animaleImage img')[0].src.split('/');
-            var imageName = tempArr[tempArr.length-1];
-            var letterFromImage = imageName.split('_')[1].split('.')[0];
-            var letter = imageLocation.split('/')[2].split('.')[0];
-            if(letter === letterFromImage )
-            {
-
-
-                var snd = new Audio("./sounds/success.wav");
-                snd.play();
-                var score = parseInt($('.smallBox.scoreNumber h1').text());
-                $('.smallBox.scoreNumber').fadeIn(500).fadeOut(500).fadeIn(500).fadeOut(500).fadeIn(500).fadeOut(500).fadeIn(500);
-                score += 1;
-                $('.smallBox.scoreNumber').css( "color", "green");
-                $('.smallBox.scoreNumber h1').text(score);
-                setTimeout(function(){
-                    $('.smallBox.scoreNumber').css( "color", "black");
-
-                },4000)
-
-                //clear enlarged letter
-                if($('.enlargedLatter img').length > 0)
-                {
-                    $('.enlargedLatter img').fadeIn(500).fadeOut(500).fadeIn(500).fadeOut(500).fadeIn(500).fadeOut(500).fadeIn(500);
-
-                }
-            }
-            else{
-
-            }*/
 
         }));
 
