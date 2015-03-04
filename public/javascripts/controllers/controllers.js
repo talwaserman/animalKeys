f9e263082cb5c7f73fe856a758d9a8e9fdb9004b
tal waserman <tal.waserman@sap.com>
2015-03-04 10:41:00 +0200

    last changes

--- public/javascripts/controllers/controllers.js
+++ public/javascripts/controllers/controllers.js
@@ -40,7 +40,36 @@ mainApp.controller('completeTheWordCTR',['$scope','$http','sounds','util', funct
                 }
                 if($('.completeWord img').length === 0){
                     $('.completeWord').removeClass('completeWordContainer');
+                    $('.completeWord').removeClass('green');
+                    $('.completeWord').removeClass('red');
                 }
+                else
+                {
+                    //Get image name;
+                    var image = $('.animaleImage img')[0].src.split('/');
+                    image = image[image.length-1];
+
+                    tempObject = {
+                        "image": image,
+                        "letters": wordEntered
+                    };
+
+                    if(_checkWord(tempObject))
+                    {
+                        sounds.success();
+                        $('.completeWord').fadeIn(500).fadeOut(500).fadeIn(500).fadeOut(500).fadeIn(500);
+                        var score = parseInt($('.smallBox.scoreNumber h1').text());
+                        $('.smallBox.scoreNumber').fadeIn(500).fadeOut(500).fadeIn(500).fadeOut(500).fadeIn(500).fadeOut(500).fadeIn(500);
+                        score += 1;
+                        $('.smallBox.scoreNumber').css( "color", "green");
+                        $('.smallBox.scoreNumber h1').text(score);
+                        setTimeout(function(){
+                            $('.smallBox.scoreNumber').css( "color", "black");
+
+                        },4000)
+                    }
+                }
+
             break;
 
             default:
@@ -88,12 +117,18 @@ mainApp.controller('completeTheWordCTR',['$scope','$http','sounds','util', funct
                 if(allWords[i].image === array.image)
                 {
                     bool = true;
-                    for(var j = 0;j<allWords[i].letters.length ;j++)
+                    $('.completeWord').addClass('green');
+                    $('.completeWord').removeClass('red');
+                    for(var j = 0;j<array.letters.length ;j++)
                     {
                         if(allWords[i].letters[j] !== array.letters[j])
+                        {
                             bool = false;
+                            $('.completeWord').addClass('red');
+                            $('.completeWord').removeClass('green');
+                        }
                     }
-                    if(bool)
+                    if(bool && array.letters.length === allWords[i].letters.length)
                         return bool;
                     else
                         bool = false;
@@ -101,124 +136,7 @@ mainApp.controller('completeTheWordCTR',['$scope','$http','sounds','util', funct
             }
             return false;
         }
-    }
-    $(function(){
-
-        /*$http.get('javascripts/controllers/words.json').success(function(data) {
-            allWords = data;
-        });*/
-
-        $('.keyboard li').on('tap',(function(event){
-
-           /* if(event.target.className.split(" ")[0] === "space")
-            {
-                sounds.swipe();
-
-                if($('.animaleImage img').length > 0)
-                {
-                    $('.animaleImage img').remove();
-
-                    var newimage = $('<img />').attr({
-                        src: "/images/animales/"+imageList[imageIndex]
-                    }).css({width:448,height:285 });
-
-                    $('.animaleImage').append(newimage);
-
-                    if($('.completeWord img').length > 0)
-                    {
-                        $('.completeWord img').remove();
-                    }
-                }
-
-                imageIndex++;
-                wordEntered = [];
-            }*/
-            /*else if(event.target.className.split(" ")[0] === "backSpace"){
-                if($('.completeWord img').length > 0)
-                {
-                    $('.completeWord img')[$('.completeWord img').length-1].remove();
-                    wordEntered.pop();
-                }
-                if($('.completeWord img').length === 0){
-                    $('.completeWord').removeClass('completeWordContainer');
-                }
-
-            }*/
-            /*else if(event.target.className.split(" ")[0] === "enter"){
-                $scope.wordsArr.push(
-                    {
-                        "image": imageList[imageIndex-1],
-                        "letters": wordEntered
-                    });
-                wordEntered = [];
-            }*/
-            /*else{
-                //Letter pressed
-                $('.completeWord').addClass('completeWordContainer');
-                sounds.letter("./voice/"+event.target.src.split('/')[5].split('.')[0]+".mp3");
-
-                var tempObject ,
-                length = event.target.src.split("/").length,
-                letter = event.target.src.split("/")[length-1];
-
-                //Update word on screen
-                var imageLocation =  "images/letters/"+letter;
-                var div = $('<img src="'+imageLocation+'" style="float:right;width:60px;height:60px">');
-                $('.completeWord').append(div);
-
-                wordEntered.push(letter);
-
-                //Get image name;
-                var image = $('.animaleImage img')[0].src.split('/');
-                image = image[image.length-1];
-
-                tempObject = {
-                    "image": image,
-                    "letters": wordEntered
-                };
-
-                if(_checkWord(tempObject))
-                {
-                    sounds.success();
-                    $('.completeWord').fadeIn(500).fadeOut(500).fadeIn(500).fadeOut(500).fadeIn(500);
-                    var score = parseInt($('.smallBox.scoreNumber h1').text());
-                    $('.smallBox.scoreNumber').fadeIn(500).fadeOut(500).fadeIn(500).fadeOut(500).fadeIn(500).fadeOut(500).fadeIn(500);
-                    score += 1;
-                    $('.smallBox.scoreNumber').css( "color", "green");
-                    $('.smallBox.scoreNumber h1').text(score);
-                    setTimeout(function(){
-                        $('.smallBox.scoreNumber').css( "color", "black");
-
-                    },4000)
-                }
-            }*/
-        }));
-
-
-        /*function _getRandomImage(){
-            return Math.floor(Math.random()*imageList.length)+1;
-        }
-
-        function _checkWord(array) {
-            var bool;
-            for (var i = 0;i<allWords.length  ;i++) {
-                if(allWords[i].image === array.image)
-                {
-                    bool = true;
-                    for(var j = 0;j<allWords[i].letters.length ;j++)
-                    {
-                        if(allWords[i].letters[j] !== array.letters[j])
-                            bool = false;
-                    }
-                    if(bool)
-                        return bool;
-                    else
-                        bool = false;
-                }
-            }
-            return false;
-        }*/
-    });
+    } //keyPress
 }]);
 mainApp.controller('knowTheLettersCTR',['$scope','sounds', function($scope, sounds) {
     $scope.keyPressed = function(letter){
@@ -257,7 +175,7 @@ mainApp.controller('firstLetterCTR',['$scope', 'sounds','util', function($scope,
                 sounds.swipe();
             }
         }
-        else{
+        else if(letter !== 'backSpace'){
             sounds.letter("./voice/"+letter.split('.')[0]+".mp3");
 
             if($('.enlargedLatter img').length > 0)
